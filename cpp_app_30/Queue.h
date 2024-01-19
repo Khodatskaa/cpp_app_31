7d08d67a8f54a2b707659229281b9483190e4dd1
Khodatskaa <khodatskaalina@gmail.com>
2024-01-19 20:16:05 +0200

    Ex 1 was done

--- cpp_app_30/Queue.h
+++ cpp_app_30/Queue.h
@@ -1,1 +1,26 @@
-#pragma once
+#ifndef Queue_h
+#define Queue_h
+
+#include <iostream>
+#include <string>	
+
+template <class T>
+class Queue{
+private:
+    class Node;
+
+    Node* front;
+    Node* back;
+
+public:
+    Queue();
+
+    void queue(const T& value);
+    void queue();
+    T frontElement() const;
+    bool isEmpty() const;
+
+    ~Queue();
+};
+
+#endif 
